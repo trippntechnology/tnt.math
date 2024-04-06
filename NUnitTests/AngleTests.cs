06b21aab330e8b93d0dd70f2f01ed6b119605467
Steve <stevetripp@comcast.net>
2024-04-06 14:05:08 -0600

    .NET8

---
+++ NUnitTests/AngleTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TNT.Math.Tests
+{
+  [ExcludeFromCodeCoverage]
+  public class AngleTests
+  {
+    [Test]
+    public void ConstructorTests()
+    {
+      //try
+      //{
+      //  Angle a1;
+      //  Assert.Throws<ArgumentException>(delegate { a1 = new Angle(-1.1); });
+      //  Assert.Throws<ArgumentException>(delegate { a1 = new Angle(1.1); });
+      //  a1 = new Angle(-1.1);
+      //}
+      //catch (ArgumentException ae)
+      //{
+      //  Assert.That("The angle parameter must be between 1 and -1", ae.Message);
+      //}
+
+      Angle a1 = new Angle(System.Math.PI);
+      Assert.That(System.Math.PI, Is.EqualTo(a1.InRadians));
+      Assert.That(180, Is.EqualTo(a1.InDegrees));
+
+      Angle a2 = new Angle(90, true);
+      Assert.That(System.Math.PI / 2, Is.EqualTo(a2.InRadians));
+      Assert.That(90, Is.EqualTo(a2.InDegrees));
+
+      Angle a3 = new Angle(System.Math.PI / 4, false);
+      Assert.That(System.Math.PI / 4, Is.EqualTo(a3.InRadians));
+      Assert.That(45, Is.EqualTo(a3.InDegrees));
+
+      Angle a4 = new Angle(a3);
+      Assert.That(System.Math.PI / 4, Is.EqualTo(a4.InRadians));
+      Assert.That(45, Is.EqualTo(a4.InDegrees));
+    }
+
+    [Test]
+    public void EmptyTest()
+    {
+      Assert.That(new Angle(0), Is.EqualTo(Angle.Empty));
+    }
+
+    [Test]
+    public void ToStringTest()
+    {
+      Angle a1 = new Angle(247, true);
+      Assert.That("247", Is.EqualTo(a1.ToString()));
+
+      Angle a2 = new Angle(12.34, true);
+      Assert.That("12.34", Is.EqualTo(a2.ToString()));
+    }
+
+    [Test]
+    public void OperatorAddTest()
+    {
+      Angle a1 = new Angle(33, true);
+      Angle a2 = new Angle(42, true);
+
+      Angle sum = a1 + a2;
+
+      Assert.That(75, Is.EqualTo(System.Math.Round(sum.InDegrees, 3)));
+    }
+
+    [Test]
+    public void NotEqual_Not_Angle()
+    {
+      Angle a1 = new Angle(33, true);
+      Assert.That(a1.Equals(1), Is.False);
+    }
+
+    [Test]
+    public void GetHash()
+    {
+      var a1 = new Angle(33, true);
+      var hash = a1.GetHashCode();
+      Assert.That(hash, Is.EqualTo(-393782082));
+    }
+  }
+}
---
+++ NUnitTests/VectorTests.cs
@@ -0,0 +1,265 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+
+namespace TNT.Math.Tests
+{
+  [ExcludeFromCodeCoverage]
+  public class VectorTests
+  {
+    [Test]
+    public void ConstructorTests()
+    {
+      Vector v1 = new Vector(1.234, 5.678);
+      Assert.That(1.234, Is.EqualTo(v1.X));
+      Assert.That(5.678, Is.EqualTo(v1.Y));
+
+      Vector v2 = new Vector(v1);
+      Assert.That(v1, Is.EqualTo(v2));
+
+      Vector v3 = new Vector(new PointF(0, 0), new PointF(10, 10));
+      Assert.That(new Vector(10, 10), Is.EqualTo(v3));
+
+      Assert.That(10, Is.EqualTo(v3.X));
+      Assert.That(10, Is.EqualTo(v3.Y));
+
+      Vector v4 = new Vector(new PointF(13, 17));
+
+      Assert.That(new Vector(13, 17), Is.EqualTo(v4));
+    }
+
+    [Test]
+    public void ConstructorTest_Angle()
+    {
+      Vector v = new Vector(new Angle(0, true), System.Math.Sqrt(100));
+      Assert.That(0, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(-10, Is.EqualTo(System.Math.Round(v.Y, 0)));
+
+      v = new Vector(new Angle(45, true), System.Math.Sqrt(200));
+      Assert.That(10, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(-10, Is.EqualTo(System.Math.Round(v.Y, 0)));
+
+      v = new Vector(new Angle(90, true), System.Math.Sqrt(100));
+      Assert.That(10, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(0, Is.EqualTo(System.Math.Round(v.Y, 0)));
+
+      v = new Vector(new Angle(135, true), System.Math.Sqrt(200));
+      Assert.That(10, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(10, Is.EqualTo(System.Math.Round(v.Y, 0)));
+
+      v = new Vector(new Angle(180, true), System.Math.Sqrt(100));
+      Assert.That(0, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(10, Is.EqualTo(System.Math.Round(v.Y, 0)));
+
+      v = new Vector(new Angle(225, true), System.Math.Sqrt(200));
+      Assert.That(-10, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(10, Is.EqualTo(System.Math.Round(v.Y, 0)));
+
+      v = new Vector(new Angle(270, true), System.Math.Sqrt(100));
+      Assert.That(-10, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(0, Is.EqualTo(System.Math.Round(v.Y, 0)));
+
+      v = new Vector(new Angle(315, true), System.Math.Sqrt(200));
+      Assert.That(-10, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(-10, Is.EqualTo(System.Math.Round(v.Y, 0)));
+
+      v = new Vector(new Angle(360, true), System.Math.Sqrt(100));
+      Assert.That(0, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(-10, Is.EqualTo(System.Math.Round(v.Y, 0)));
+
+      v = new Vector(new Angle(-45, true), System.Math.Sqrt(200));
+      Assert.That(-10, Is.EqualTo(System.Math.Round(v.X, 0)));
+      Assert.That(-10, Is.EqualTo(System.Math.Round(v.Y, 0)));
+    }
+
+    [Test]
+    public void MagnitudeTest()
+    {
+      Vector v1 = new Vector(0, 17);
+      Assert.That(v1.Magnitude, Is.EqualTo(17));
+
+      Vector v2 = new Vector(13, 0);
+      Assert.That(v2.Magnitude, Is.EqualTo(13));
+
+      Vector v3 = v1 + v2;
+      Assert.That(new Vector(13, 17), Is.EqualTo(v3));
+
+      Assert.That(System.Math.Round(v3.Magnitude, 5), Is.EqualTo(21.40093));
+
+      Vector v4 = new Vector(8, 12);
+      Assert.That(v3 - v4, Is.EqualTo(new Vector(5, 5)));
+      Assert.That(v4 - v3, Is.EqualTo(new Vector(-5, -5)));
+    }
+
+    [Test]
+    public void DotTest()
+    {
+      Vector v1 = new Vector(7, 13);
+      Vector v2 = new Vector(17, 23);
+
+      Assert.That(v1.Dot(v2), Is.EqualTo(7 * 17 + 13 * 23));
+    }
+
+    [Test]
+    public void OperatorDotTest()
+    {
+      Vector v1 = new Vector(7, -13);
+      Vector v2 = new Vector(17, 23);
+
+      Assert.That(v1.Dot(v2), Is.EqualTo(v1 * v2));
+      Assert.That(v2.Dot(v1), Is.EqualTo(v2 * v1));
+    }
+
+    [Test]
+    public void UnitVectorTest()
+    {
+      Vector v1 = new Vector(0, 0);
+      Assert.That(v1.Unit, Is.EqualTo(new Vector(0, 0)));
+
+      Vector v2 = new Vector(25, 0);
+      Assert.That(v2.Unit, Is.EqualTo(new Vector(1, 0)));
+
+      Vector v3 = new Vector(0, 25);
+      Assert.That(v3.Unit, Is.EqualTo(new Vector(0, 1)));
+
+      Vector v4 = new Vector(25, 25);
+      Assert.That(System.Math.Round(v4.Unit.X, 3), Is.EqualTo(System.Math.Round(.70710, 3)));
+      Assert.That(System.Math.Round(v4.Unit.Y, 3), Is.EqualTo(System.Math.Round(.70710, 3)));
+    }
+
+    [Test]
+    public void ToStringTest()
+    {
+      Vector v1 = new Vector(13.321, 17.754);
+      Assert.That(v1.ToString(), Is.EqualTo("[13.321 17.754]"));
+
+      Vector v2 = new Vector(.321, .565);
+      Assert.That(v2.ToString(), Is.EqualTo("[0.321 0.565]"));
+    }
+
+    [Test]
+    public void OperatorMultiplyTest()
+    {
+      Vector v1 = new Vector(3, 7);
+
+      Assert.That(v1 * 7, Is.EqualTo(new Vector(21, 49)));
+      Assert.That(9 * v1, Is.EqualTo(new Vector(27, 63)));
+    }
+
+    [Test]
+    public void OperatorAddTest()
+    {
+      PointF pf1 = new PointF(13, 17);
+      Point p1 = new Point(13, 17);
+      Vector v1 = new Vector(-7, -23);
+
+      Assert.That(v1 + pf1, Is.EqualTo(new PointF(6, -6)));
+      Assert.That(pf1 + v1, Is.EqualTo(new PointF(6, -6)));
+
+      Point p1Ex = new Point(6, -6);
+      Assert.That(v1 + p1, Is.EqualTo((PointF)p1Ex));
+      Assert.That(p1 + v1, Is.EqualTo((PointF)p1Ex));
+    }
+
+    [Test]
+    public void CastingTests()
+    {
+      Vector v1 = new Vector(1.234, 5.678);
+      PointF pf1 = v1;
+      Point p1 = v1;
+
+      Assert.That(pf1, Is.EqualTo(new PointF((float)1.234, (float)5.678)));
+      Assert.That(p1, Is.EqualTo(new Point(1, 5)));
+    }
+
+    [Test]
+    public void AngleTests()
+    {
+      Vector v0 = new Vector(0, 0);
+      Assert.That(v0.Angle(v0), Is.EqualTo(new Angle(0)));
+
+      Vector v1 = new Vector(0, -10);
+      Assert.That(v1.Angle(v0), Is.EqualTo(Angle.Empty));
+      Assert.That(v0.Angle(v1), Is.EqualTo(Angle.Empty));
+
+      for (double theta = 0; theta <= 180; theta += 22.5)
+      {
+        double thetaRad = theta / 180 * System.Math.PI;
+        Vector v = new Vector(10 * System.Math.Sin(thetaRad), -10 * System.Math.Cos(thetaRad));
+
+        Assert.That(theta, Is.EqualTo(System.Math.Round(v.Angle(v1).InDegrees, 1)));
+      }
+
+      for (double theta = 0; theta >= -180; theta -= 22.5)
+      {
+        double thetaRad = theta / 180 * System.Math.PI;
+        Vector v = new Vector(10 * System.Math.Sin(thetaRad), -10 * System.Math.Cos(thetaRad));
+
+        Assert.That(-theta, Is.EqualTo(System.Math.Round(v.Angle(v1).InDegrees, 1)));
+      }
+    }
+
+    [Test]
+    public void AngleTests2()
+    {
+      Vector v1 = new Vector(new Angle(33, true), 1);
+      Assert.That(33, Is.EqualTo(System.Math.Round(v1.Angle().InDegrees)));
+
+      for (int angle = 205; angle < 220; angle++)
+      {
+        Vector v = new Vector(new Angle(angle, true), 1);
+        Assert.That(angle, Is.EqualTo(System.Math.Round(v.Angle().InDegrees)));
+        Angle a = v.Angle(v1, true);
+        Assert.That(angle - 33, Is.EqualTo(System.Math.Round(a.InDegrees)));
+      }
+    }
+
+    [Test]
+    public void RotateTest()
+    {
+      Vector origVector = new Vector(1, 0);
+      Vector vector90 = origVector.Rotate(new Angle(90, true));
+      Vector vector45 = origVector.Rotate(new Angle(45, true));
+
+      Assert.That(0, Is.EqualTo(System.Math.Round(vector90.X, 3)));
+      Assert.That(1, Is.EqualTo(System.Math.Round(vector90.Y, 3)));
+
+      Assert.That(System.Math.Round(vector45.X, 3), Is.EqualTo(System.Math.Round(0.7069, 3)));
+      Assert.That(System.Math.Round(vector45.Y, 3), Is.EqualTo(System.Math.Round(0.7069, 3)));
+    }
+
+    [Test]
+    public void DivideTest()
+    {
+      Vector vector = new Vector(27, 30);
+      Vector dividend = new Vector(9, 10);
+
+      Assert.That(vector / 3, Is.EqualTo(dividend));
+    }
+
+    [Test]
+    public void SubtractTest()
+    {
+      PointF p = new PointF(21, 29);
+      Vector v = new Vector(17, 11);
+      Vector expected = new Vector(4, 18);
+
+      Assert.That((p - v).X, Is.EqualTo(expected.X));
+      Assert.That((p - v).Y, Is.EqualTo(expected.Y));
+    }
+
+    [Test]
+    public void NotThat_Not_Vector()
+    {
+      var v = new Vector(17, 11);
+      Assert.That(v.Equals(1), Is.False);
+    }
+
+    [Test]
+    public void GetHash()
+    {
+      var v = new Vector(17, 11);
+      var hash = v.GetHashCode();
+      Assert.That(hash, Is.EqualTo(-2141782003));
+    }
+  }
+}
